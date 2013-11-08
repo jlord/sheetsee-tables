5a9d22702c4251facc9fa4a646edd1f68f03fb43
jlord <jessica.lord@github.com>
2013-11-08 23:35:08 +0000

    updated with pagination

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 var ich = require('icanhaz')
 
-// Make Table, Sort and Filter Interactions
-module.exports.initiateTableFilter = function(data, filterDiv, tableDiv) {
-  $('.clear').on("click", function() {
+module.exports.initiateTableFilter = function(opts) {
+  // console.log("Orig Data is", opts.data.length)
+  // var origData = opts.data
+  $('.clear').on("click", function() { 
     $(this.id + ".noMatches").css("visibility", "hidden")
-    $(this.id + filterDiv).val("")
-    Sheetsee.makeTable(data, tableDiv)
+    $(this.id + opts.filterDiv).val("")
+    // opts.data = origData
+    makeTable(opts)
   })
-  $(filterDiv).keyup(function(e) {
+  $(opts.filterDiv).keyup(function(e) {
     var text = $(e.target).val()
-    Sheetsee.searchTable(data, text, tableDiv)
+    console.log(opts.data.length, text)
+    searchTable(opts, text)
   })
 }
 
-module.exports.searchTable = function(data, searchTerm, tableDiv) {
+module.exports.searchTable = function(opts, searchTerm) {
   var filteredList = []
-  data.forEach(function(object) {
+  opts.data.forEach(function(object) {
     var stringObject = JSON.stringify(object).toLowerCase()
     if (stringObject.match(searchTerm.toLowerCase())) filteredList.push(object)
   })
   if (filteredList.length === 0) {
-    console.log("no matchie")
     $(".noMatches").css("visibility", "inherit")
-    Sheetsee.makeTable("no matches", tableDiv)
+    makeTable(opts, filteredList)
   }
-  else $(".noMatches").css("visibility", "hidden")
-  Sheetsee.makeTable(filteredList, tableDiv)
-  return filteredList
+  else {
+    $(".noMatches").css("visibility", "hidden")
+    makeTable(opts, filteredList)
+  } 
 }
 
-module.exports.sortThings = function(data, sorter, sorted, tableDiv) {
-  data.sort(function(a,b){
+module.exports.sortThings = function(opts, sorter, sorted) {
+  console.log("here is data", opts.data)
+  opts.data.sort(function(a,b){
     if (a[sorter]<b[sorter]) return -1
     if (a[sorter]>b[sorter]) return 1
     return 0
   })
-  if (sorted === "descending") data.reverse()
-  Sheetsee.makeTable(data, tableDiv)
-  var header
-  $(tableDiv + " .tHeader").each(function(i, el){
-    var contents = Sheetsee.resolveDataTitle($(el).text())
+  if (sorted === "descending") opts.data.reverse()
+  makeTable(opts)
+  var header 
+  $(opts.tableDiv + " .tHeader").each(function(i, el){
+    var contents = resolveDataTitle($(el).text())
     if (contents === sorter) header = el
   })
   $(header).attr("data-sorted", sorted)
@@ -50,20 +54,72 @@ module.exports.resolveDataTitle = function(string) {
   return adjusted
 }
 
-module.exports.sendToSort = function(event) {
-  var tableDiv = "#" + $(event.target).closest("div").attr("id")
-  console.log("came from this table",tableDiv)
-  var sorted = $(event.target).attr("data-sorted")
-  if (sorted) {
-    if (sorted === "descending") sorted = "ascending"
-    else sorted = "descending"
+module.exports.initiateTableSorter = function(options) {
+  var sortInfo = $(document).on("click", ".tHeader", sendToSort)
+
+  function sendToSort(event) {
+    var tableDiv = "#" + $(event.target).closest("div").attr("id")
+    console.log("came from this table",tableDiv)
+    var sorted = $(event.target).attr("data-sorted")
+    if (sorted) {
+      if (sorted === "descending") sorted = "ascending"
+      else sorted = "descending"
+    }
+    else { sorted = "ascending" }
+    var sorter = resolveDataTitle(event.target.innerHTML)
+    var sortInfo = {"sorter": sorter, "sorted": sorted, "tableDiv": tableDiv}
+    console.log(sortInfo)
+    sortThings(options, sorter, sorted, tableDiv)
   }
-  else { sorted = "ascending" }
-  var sorter = Sheetsee.resolveDataTitle(event.target.innerHTML)
-  Sheetsee.sortThings(gData, sorter, sorted, tableDiv)
 }
 
-module.exports.makeTable = function(data, targetDiv) {
+module.exports.makeTable = function(opts, filteredList) {
+  if (filteredList) var data = filteredList
+    else var data = opts.data
+      
+  if (!opts.pagination) table(data, targetDiv)
+  var allRows = data.length
+  var totalPages = Math.floor(allRows / opts.pagination)
+  var currentPage = 1
+  var currentStart = (currentPage * opts.pagination) - opts.pagination
+  var currentEnd = currentPage * opts.pagination
+  var currentRows = data.slice(currentStart, currentEnd)
+  table(currentRows, opts.tableDiv)
+  if (opts.data.length > opts.pagination) setPreNext(opts.tableDiv, currentPage, currentPage, totalPages)
+  
+  $(document).on("click", (".pagination-next"), function() { 
+    // if (opts.filterDiv && $(opts.filterDiv).val().length === 0 ) console.log("unempty filter!")
+    currentPage = currentPage + 1
+    var nextPage = currentPage + 1
+    currentStart = (currentPage * opts.pagination) - opts.pagination
+    currentEnd = currentPage * opts.pagination
+    currentRows = data.slice(currentStart, currentEnd)
+    table(currentRows, opts.tableDiv)
+    setPreNext(opts.tableDiv, currentPage, currentPage, totalPages)
+  })
+
+  $(document).on("click", (".pagination-pre"), function() { 
+    currentPage = currentPage - 1
+    var nextPage = currentPage + 1
+    currentStart = (currentPage * opts.pagination) - opts.pagination
+    currentEnd = currentPage * opts.pagination
+    currentRows = data.slice(currentStart, currentEnd) 
+    table(currentRows, opts.tableDiv)
+    setPreNext(opts.tableDiv, currentPage, currentPage, totalPages)
+  })
+}
+
+module.exports.setPreNext = function(targetDiv, currentPage, currentPage, totalPages) {
+  $(targetDiv).append("<div id='Pagination' pageno='" + currentPage + "'" + "class='table-pagination'>Showing page " 
+    + currentPage + " of " + totalPages + " <a class='pagination-pre'>Previous</a>" +
+    " <a class='pagination-next'>Next</a></p></div>" )
+}
+
+module.exports.clearPreNExt = function() {
+  $(".table-pagination").attr("display", "none")
+}
+
+module.exports.table = function(data, targetDiv) {
   var templateID = targetDiv.replace("#", "")
   var tableContents = ich[templateID]({
     rows: data
